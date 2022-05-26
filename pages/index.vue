78d32da32b4bb4d3da2d27c4decc4ce4a63cf685
YusufcanY <playders03@gmail.com>
2022-05-26 17:24:26 +0300

    fix: lint fixes

--- pages/index.vue
+++ pages/index.vue
@@ -10,22 +10,22 @@
 </script>
 <template>
   <div
-    class="flex h-screen w-full flex-col items-center justify-center space-y-4 bg-gray-800 text-white"
+    class="flex flex-col items-center justify-center w-full h-screen space-y-4 text-white bg-gray-800"
   >
     <h1 class="text-4xl font-black">IT'S A TEXT</h1>
     <span class="text-2xl">{{ counter }}</span>
     <div class="flex space-x-2">
       <button
-        class="rounded-md bg-gray-600 p-2 font-semibold text-white shadow-md transition-transform duration-200 hover:-translate-y-1 active:translate-y-0"
+        class="p-2 font-semibold text-white transition-transform duration-200 bg-gray-600 rounded-md shadow-md hover:-translate-y-1 active:translate-y-0"
         @click="increment"
       >
-        <PlusCircleIcon class="h-8 w-8" />
+        <PlusCircleIcon class="w-8 h-8" />
       </button>
       <button
-        class="rounded-md bg-gray-600 p-2 font-semibold text-white shadow-md transition-transform duration-200 hover:-translate-y-1 active:translate-y-0"
+        class="p-2 font-semibold text-white transition-transform duration-200 bg-gray-600 rounded-md shadow-md hover:-translate-y-1 active:translate-y-0"
         @click="decrement"
       >
-        <MinusCircleIcon class="h-8 w-8" />
+        <MinusCircleIcon class="w-8 h-8" />
       </button>
     </div>
   </div>
